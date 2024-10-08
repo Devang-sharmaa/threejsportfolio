7715b73f25c9d65e816311b23478cbf130ec10d9
Devang-sharmaa <fantombrothers191@gmail.com>
2024-10-08 11:27:22 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myportfolio/",
+  base: "/threejsportfolio/",
   server: {
     port: 3000,
   },
